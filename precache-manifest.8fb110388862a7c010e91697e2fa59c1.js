789ad2cc77a1840e52dcc13b2de4449dde7dab29
GeorgeNicola <nicola_george1999@yahoo.com>
2020-08-24 17:08:41 +0300

    Updates

---
+++ precache-manifest.8fb110388862a7c010e91697e2fa59c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b3b98cae83723572e319938d5904d24",
+    "url": "/GeorgeNicola/face-recognition/index.html"
+  },
+  {
+    "revision": "c4dc52150a778928a346",
+    "url": "/GeorgeNicola/face-recognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "378c8bfb973d9d957f1e",
+    "url": "/GeorgeNicola/face-recognition/static/css/main.fd7e42da.chunk.css"
+  },
+  {
+    "revision": "c4dc52150a778928a346",
+    "url": "/GeorgeNicola/face-recognition/static/js/2.d02ce618.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/GeorgeNicola/face-recognition/static/js/2.d02ce618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "378c8bfb973d9d957f1e",
+    "url": "/GeorgeNicola/face-recognition/static/js/main.5085346d.chunk.js"
+  },
+  {
+    "revision": "78eb13bc2cb2da23d2ea",
+    "url": "/GeorgeNicola/face-recognition/static/js/runtime-main.8fc40838.js"
+  }
+]);
